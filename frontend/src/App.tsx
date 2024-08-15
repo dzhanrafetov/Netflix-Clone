1f65cc63ba6e7cad8b745cfd6d2bd09f672ac90c
DzhanRafetov <can.33@abv.bg>
2024-08-15 17:00:54 +0300

    HomePage/Authorization UI add

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import SignUpPage from "./pages/SignUp"
+import LoginPage from "./pages/LoginPage"
+import HomePage from "./pages/home/HomePage"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+
+      </Routes>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
