bca7fe56785c585d08f8f9186b3f1eaaedc77020
DzhanRafetov <can.33@abv.bg>
2024-08-19 09:57:20 +0300

    Initial Commit for Production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,12 @@ import { connectDB } from "./config/db";
 import cookieParser from "cookie-parser";
 import { protectRoute } from "./middlewares/protectRoute";
 import searchRoute from "./routes/searchRoute";
-
+import path from "path";
 
 const app = express()
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser())
 
@@ -17,9 +20,14 @@ app.use("/api/v1/auth", authRoute)
 app.use("/api/v1/movie", protectRoute, movieRoute);
 app.use("/api/v1/tv", protectRoute, tvRoute);
 app.use("/api/v1/search", protectRoute, searchRoute);
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+  app.get("*",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+  })
+}
 
-const PORT = ENV_VARS.PORT;
 
 app.listen(PORT, () => {
   connectDB();
